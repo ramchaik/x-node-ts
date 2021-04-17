2aea6b1a24833b445ce26a8743a34eaa8d1a80e8
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-04-17 21:30:16 +0530

    feat: add crud operations for todos

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -11,3 +11,39 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.json({ message: "Created the todo.", createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = getTodoIndex(todoId);
+
+  TODOS[todoIndex] = new Todo(todoId, updatedText);
+
+  res.json({
+    message: "Todo updated successfully.",
+    updatedTodo: TODOS[todoIndex],
+  });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = getTodoIndex(todoId);
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "Todo deleted successfully" });
+};
+
+function getTodoIndex(todoId: string) {
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+  return todoIndex;
+}
